60415303e692216796ff6ea879387335945bc47c
Albina <shakirova.1910@mail.ru>
2022-12-15 15:33:25 +0300

    dz_36

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
-﻿/* Задача 34: Задайте массив заполненный случайными 
-положительными трёхзначными числами. 
-Напишите программу, которая покажет 
-количество чётных чисел в массиве.*/
+﻿/* Задача 36
+ Задайте одномерный массив, заполненный 
+случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.*/
 
-int[] array = new int [3];
-for (int i = 0;i < array.Length ; i++)
-    array[i] = new Random().Next(100,1000);
+int[] array = new int [7];
+for (int i = 0; i < array.Length ; i++)
+    array[i] = new Random().Next(10);
 int k = 0;
-for (int i = 0; i < array.Length; i++)
-    if (array[i] % 2 == 0)
-        k++;
+for (int i = 1; i < array.Length; i += 2)
+        k+= array[i];
         
 for (int i = 0; i < array.Length; i++)
     Console.Write($"{array[i]} ");
