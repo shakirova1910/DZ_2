29e849f71840f2d30a40d046088ff389156f274f
Albina <shakirova.1910@mail.ru>
2022-12-20 15:25:44 +0300

    dz_50

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
-﻿/* Задача 47. Задайте двумерный массив размером m×n, 
-заполненный случайными вещественными числами. */
+﻿/* Задача 50. Напишите программу, которая на вход принимает 
+позиции элемента в двумерном массиве, и возвращает значение 
+этого элемента или же указание, что такого элемента нет.
+ */
 
-int m, n;
-Console.WriteLine("введите значение m: ");
-int.TryParse(Console.ReadLine(), out m);
-Console.WriteLine("введите значение n: ");
-int.TryParse(Console.ReadLine(), out n);
-
-int[,] array = new int [m, n];
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
  
-for (int i = 0; i < array.GetLength(0); i++)
-{   
+for (int i = 0; i < array.GetLength(0); i++) 
+{
     for (int j = 0; j < array.GetLength(1); j++)
-    {
-        array[i, j] = new Random() .Next(-9, 10);
-        Console.Write($"{array[i, j]} ");
-    } 
-    
-Console.WriteLine();
-    }
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
+
 
