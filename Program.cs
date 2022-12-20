8e1e1c00397baf6fe9863968f14ea588db92e4a4
Albina <shakirova.1910@mail.ru>
2022-12-20 15:57:55 +0300

    dz_52

--- Program.cs
+++ Program.cs
@@ -1,35 +1,37 @@
-﻿/* Задача 50. Напишите программу, которая на вход принимает 
-позиции элемента в двумерном массиве, и возвращает значение 
-этого элемента или же указание, что такого элемента нет.
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
  */
 
-Console.WriteLine("Введите размеры массива");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
- 
-for (int i = 0; i < array.GetLength(0); i++) 
+int [,] array = new int [3,4];
+
+for (int i = 0; i<array.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+    for (int j = 0; j<array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(10);
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
 }
- 
-for (int i = 0; i < array.GetLength(0); i++) 
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-        Console.Write(array[i,j] + "\t  ");
-        Console.WriteLine();
-}
- 
- Console.WriteLine("Введите координаты");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a>m && b>n)
- Console.WriteLine("такого числа нет");
- else
- {
- object c = array.GetValue(a,b);
- Console.WriteLine(c);
- }
 
 
+
+
+
+;
+for (int j = 0; j<array.GetLength(0); j++)
+{
+        int sum = 0;
+        for (int i = 0; i<array.GetLength(0); i++)
+            sum += array [i, j];
+    Console.Write($"{Math.Round((double)sum/array.GetLength(0), 1)} ");
+}
+
+
+
+/*for (int i = 0; i<array.GetLength(0); i++)
+{
+    for (int j = 0; j<array.GetLength(1); j++)
+        Console.Write($"{array[i,j]} ");
+    Console.WriteLine();
+}*/
